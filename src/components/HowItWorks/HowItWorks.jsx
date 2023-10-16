f00ab65d15e601e4225b3467ead55a3d58f73da8
asif ramzan <asiframzan2021@gmail.com>
2023-10-16 08:06:59 +0500

    how it works updagted

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -5,25 +5,32 @@ import { Typography, Grid } from "@mui/material";
 const HowItWorks = () => {
   return (
     <div className="howItWorks_outer">
-      <div className="howItWorks_inner">
-        <Typography variant="h3">How it Works</Typography>
-        <Typography variant="p">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </Typography>
-      </div>
+      <Typography variant="h3">How it Works</Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </Typography>
 
-      <div>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <div>
-              <img src="./images/illustration.png" alt="illustration" />
-            </div>
-          </Grid>
-          <Grid item xs={4}>
-            <div>xs=4</div>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <img src="./images/illustration.png" alt="illustration" />
         </Grid>
-      </div>
+        <Grid md={1}>
+        <img src="./images/blue_check.svg" alt="svg" />
+        </Grid>
+
+        <Grid item xs={12} md={3}>
+          <span className="howItWorks_step">STEP 1</span>
+          <div>
+            <span className="howItWorks_step_span">
+             <Typography variant="h4"> Search Flights</Typography>
+            </span>
+            <Typography variant="p">
+            
+              Enter flight destination and dates. Click search
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
